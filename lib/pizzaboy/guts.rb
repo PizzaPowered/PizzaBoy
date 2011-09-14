21711f01ca9b8bf60bd1765e314358477a2e5a5b
Caius Durling <dev@caius.name>
2011-09-15 00:49:17 +0100

    Moar guts

--- lib/pizzaboy/guts.rb
+++ lib/pizzaboy/guts.rb
@@ -4,4 +4,15 @@ PizzaBoy.guts do
     say "DAMN YOU WILL JESSOP"
   end
 
+  match /^\s*f+u+\s*$/i do
+    say "http://caius.name/images/fu.jpg"
+  end
+
+  # ==================
+  # = Debug triggers =
+  # ==================
+  match /^say (.+)$/ do
+    say matches[0]
+  end
+
 end
